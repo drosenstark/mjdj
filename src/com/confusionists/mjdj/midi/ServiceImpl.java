247b230c524b47be39855b281df5ab1a64826e5f
Daniel Rosenstark <github@confusionists.com>
2011-05-01 16:44:28 -0400

    master Fixed service to handle pitch bend

--- src/com/confusionists/mjdj/midi/ServiceImpl.java
+++ src/com/confusionists/mjdj/midi/ServiceImpl.java
@@ -7,7 +7,7 @@ This program is free software: you can redistribute it and/or modify it under th
 See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>. 
 
 You may contact the author at mjdj_midi_morph [at] confusionists.com
-*/
+ */
 package com.confusionists.mjdj.midi;
 
 import java.util.ArrayList;
@@ -30,55 +30,47 @@ import com.confusionists.mjdjApi.util.MjdjService;
 
 public class ServiceImpl implements MjdjService {
 
-    public static ServiceImpl instance = new ServiceImpl();
+	public static ServiceImpl instance = new ServiceImpl();
 
-    
-    public void morph(MessageWrapper msgWrapper, String from) {
-        Universe.instance.midiDriverManager.sendToMorphs(msgWrapper, from);
+	public void morph(MessageWrapper msgWrapper, String from) {
+		Universe.instance.midiDriverManager.sendToMorphs(msgWrapper, from);
 	}
-    
+
 	public void morph(MessageWrapper message, String from, Morph avoidMorph) {
-        Universe.instance.midiDriverManager.sendToMorphs(message, from, avoidMorph);
+		Universe.instance.midiDriverManager.sendToMorphs(message, from, avoidMorph);
 	}
-    
 
-    public void send(MessageWrapper message) {
-        send(message, (String)null);
-    }
+	public void send(MessageWrapper message) {
+		send(message, (String) null);
+	}
 
-    
-    
-    public void send(MessageWrapper message, String to) {
-        List<String> tos = null;
-        if (to != null) {
-            tos = new ArrayList<String>();
-            tos.add(to);
-        }
-        send(message,tos);
-    }
-    
+	public void send(MessageWrapper message, String to) {
+		List<String> tos = null;
+		if (to != null) {
+			tos = new ArrayList<String>();
+			tos.add(to);
+		}
+		send(message, tos);
+	}
 
-    /* if to is null send to all */
-    public void send(MessageWrapper message, List<String> to) {
-        for (DeviceWrapper device : Universe.instance.midiDriverManager.outputReceivers) {
-            ReceiverDeviceWrapper receiver = (ReceiverDeviceWrapper) device;
-            if (receiver.isActive() && (to == null || to.contains(receiver.toString()))) {
-                receiver.send(message);
-                Logger.log(message, receiver);
-            }
-        }
-    }
-    
-    
+	/* if to is null send to all */
+	public void send(MessageWrapper message, List<String> to) {
+		for (DeviceWrapper device : Universe.instance.midiDriverManager.outputReceivers) {
+			ReceiverDeviceWrapper receiver = (ReceiverDeviceWrapper) device;
+			if (receiver.isActive() && (to == null || to.contains(receiver.toString()))) {
+				receiver.send(message);
+				Logger.log(message, receiver);
+			}
+		}
+	}
 
-    public void send(byte[] bytes) throws InvalidMidiDataException {
-        send(bytes, null);
-    }
+	public void send(byte[] bytes) throws InvalidMidiDataException {
+		send(bytes, null);
+	}
 
-    public void send(byte[] bytes, List<String> to)
-            throws InvalidMidiDataException {
-        send(MessageWrapper.newInstance(bytes), to);
-    }
+	public void send(byte[] bytes, List<String> to) throws InvalidMidiDataException {
+		send(MessageWrapper.newInstance(bytes), to);
+	}
 
 	public void debugLog(String text) {
 		Logger.debugLog(text);
@@ -87,69 +79,69 @@ public class ServiceImpl implements MjdjService {
 	public void debugLog(String text, Exception e) {
 		Logger.debugLog(text, e);
 	}
-    
-    
-    public void log(String text) {
-    	Logger.log(text);
-    }
 
-    public void log(String string, Exception e) {
-    	Logger.log(string, e);
+	public void log(String text) {
+		Logger.log(text);
+	}
 
-    }
+	public void log(String string, Exception e) {
+		Logger.log(string, e);
 
-    public boolean isMorphActive(String name) {
-        boolean retVal = false;
-        MorphCheckbox box = MorphCheckboxList.instance.getCheckbox(name);
-        if (box == null) {
-        	Logger.log("Error getting status (returning false)! Morpher not found " + name);
-        } else {
-            retVal = box.isSelected();
-        }
-        return retVal;
-    }
+	}
 
-    public void setMorphActive(String name, boolean status) {
-        MorphCheckbox box = MorphCheckboxList.instance.getCheckbox(name);
-        if (box == null) {
-        	Logger.log("Error setting status! Morpher not found " + name);
-        } else {
-            if (box.isSelected() != status) {
-                box.doClick();
-            }
-        }
-    }
+	public boolean isMorphActive(String name) {
+		boolean retVal = false;
+		MorphCheckbox box = MorphCheckboxList.instance.getCheckbox(name);
+		if (box == null) {
+			Logger.log("Error getting status (returning false)! Morpher not found " + name);
+		} else {
+			retVal = box.isSelected();
+		}
+		return retVal;
+	}
 
-    public void sendKeystrokes(String keystrokes) {
-        try {
-        		System.out.println("Mjdj sending keystrokes " + keystrokes);
-            new Keyboard().type(keystrokes);
-        } catch (Exception e) {
-        	Logger.log("Error sending keystrokes " + keystrokes, e);
-        }
+	public void setMorphActive(String name, boolean status) {
+		MorphCheckbox box = MorphCheckboxList.instance.getCheckbox(name);
+		if (box == null) {
+			Logger.log("Error setting status! Morpher not found " + name);
+		} else {
+			if (box.isSelected() != status) {
+				box.doClick();
+			}
+		}
+	}
 
-    }
+	public void sendKeystrokes(String keystrokes) {
+		try {
+			System.out.println("Mjdj sending keystrokes " + keystrokes);
+			new Keyboard().type(keystrokes);
+		} catch (Exception e) {
+			Logger.log("Error sending keystrokes " + keystrokes, e);
+		}
 
-    public boolean schedule(MidiTimerTask task, int beatsBeforeLaunch) {
-        return new BeatLockedTimerTask().setTask(task, beatsBeforeLaunch);
-    }
+	}
 
-    public  boolean schedule(MidiTimerTask task, int beatsBeforeLaunch, float delayAfterBeat) {
-    		if (!(delayAfterBeat < 1 || delayAfterBeat < 0) ) {
-    			log(task.getMorph().getName() + " is trying to schedule something with a delayAfterBeat greater than 1 or less than zero. Task not scheduled.");
-    			return false;
-    		}
-       return new BeatLockedTimerTask().setTask(task, beatsBeforeLaunch, delayAfterBeat);
-    }
-    
+	public boolean schedule(MidiTimerTask task, int beatsBeforeLaunch) {
+		return new BeatLockedTimerTask().setTask(task, beatsBeforeLaunch);
+	}
+
+	public boolean schedule(MidiTimerTask task, int beatsBeforeLaunch, float delayAfterBeat) {
+		if (!(delayAfterBeat < 1 || delayAfterBeat < 0)) {
+			log(task.getMorph().getName()
+					+ " is trying to schedule something with a delayAfterBeat greater than 1 or less than zero. Task not scheduled.");
+			return false;
+		}
+		return new BeatLockedTimerTask().setTask(task, beatsBeforeLaunch, delayAfterBeat);
+	}
+
 	public boolean scheduleInMs(MidiTimerTask task, int delay) {
 		Universe.instance.centralTimer.schedule(task, delay);
 		return true;
 	}
-    
+
 	public float getAfterBeat() {
 		return Universe.instance.clockHandler.getSinceLastBeat();
-		
+
 	}
 
 	public boolean isDebug() {
@@ -165,9 +157,5 @@ public class ServiceImpl implements MjdjService {
 			return -1;
 		return 0;
 	}
-
-
-	
-
 
 }
